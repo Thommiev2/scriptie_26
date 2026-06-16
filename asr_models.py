094c6b4f38177fb309d73e799b71572533a955ed
Thomas Vermet <thomasvermet@hotmail.com>
2026-06-16 13:47:21 +0200

    split up the different models into different files with different envs

--- asr_models.py
+++ asr_models.py
@@ -1,233 +1,88 @@
-import transformers
-if transformers.__version__ != "5.9.0":
-    raise ImportError("The models in asr_models require version 5.9.0 of the transformer library.\n"
-                      "Run 'python transformer_version --upgrade' to install the correct version.")
+# import transformers
+# if transformers.__version__ != "4.57.6":
+#     raise ImportError("The models in 'asr_models_qwen' require version 4.57.6 of the transformer library.\n"
+#                       "Run 'python transformer_version' to install the correct version.")
 
-import torch
-from nemo.collections.asr.models import ASRModel
-from transformers import AutoProcessor, AutoModelForSpeechSeq2Seq, pipeline, CohereAsrForConditionalGeneration
-from data_initiation import DS
-import time
+import os
+from qwen_asr import Qwen3ASRModel
 from faster_whisper import WhisperModel
+import torch
+from base_model import BaseModel, VadModel, CONFIG
+import time
+from pathlib import Path
 
-class BaseModel:
-    def __init__(self, name, model, processor=None):
-
-        # variables
-        self.name = name
-        self.model = model
-        self.processor = processor
-
-    def transcribe(self, dataset: 'DS') -> (dict[str: str], dict[str: float]):
-        pass
-
-
-# CONSTANTS
-sample_rate = 16000
-batch_size = 1
-max_new_tokens = 256
-language = 'du'
-torch_type = torch.bfloat16 if torch.cuda.is_available() else torch.bfloat16
-device = "cuda:0" if torch.cuda.is_available() else "cpu"
+#
+#
+#       Qwen3Asr uses a different version of the transformers architecture.
+#
+#
 
 
-# --------------- Cohere Labs Transcribe ----------------
-class CohereAsr(BaseModel):
+# --------------- Qwen3 ASR 1.7B ----------------
+class QwenAsr(BaseModel):
     def __init__(self):
+
+        # if transformers.__version__ == ""
+
         super().__init__(
-            name="Cohere Labs Transcribe",
-            model=CohereAsrForConditionalGeneration.from_pretrained(
-                "CohereLabs/cohere-transcribe-03-2026",
-                device_map=device,
-                dtype=torch_type
-            ),
-            processor=AutoProcessor.from_pretrained("CohereLabs/cohere-transcribe-03-2026")
+                name="Qwen3 ASR 1.7B",
+                model=Qwen3ASRModel.from_pretrained(
+                    "Qwen/Qwen3-ASR-1.7B",
+                    dtype=CONFIG['dtype'],
+                    device_map=CONFIG['device'],
+                    max_inference_batch_size=4,
+                    max_new_tokens=CONFIG['max_new_tokens']
+                )
         )
+        self.vad: VadModel | None = None
+        print('[QWN] v  Model initialized and loaded in succesfully')
 
-    def transcribe(self, dataset: 'DS') -> (dict[str: str], dict[str: float]):
+    def transcribe(self, data_file: dict) -> (str, float):
 
-        transcripts = {}
-        times = {}
+        timer = time.perf_counter()
+        print(f"[QWN] >  Attempting to transcribe {data_file['name']} from dataset {data_file['category']}")
 
-        for row in dataset.data:
-            timer = time.perf_counter()
-            print(f"> Attempting to transcribe {row['name']} from dataset {dataset.name}")
-            audio = row['audio']['array']
-            inputs = self.processor(audio, sampling_rate=row['audio']['sampling_rate'], return_tensors='pt', language='nl')
-            audio_chunk_index = inputs.get("audio_chunk_index")
-            with torch.no_grad():
-                safe_inputs = {}
-                for k, v in inputs.items():
-                    if torch.is_tensor(v):
-                        if torch.is_floating_point(v):
-                            safe_inputs[k] = v.to(self.model.device, dtype=self.model.dtype)
-                        else:
-                            safe_inputs[k] = v.to(self.model.device)
-                    else:
-                        safe_inputs[k] = v
+        chunks = self.vad.get_speech_chunks(data_file['audio'], CONFIG['sample_rate'])
+        inputs = list(zip(chunks, [CONFIG['sample_rate']] * len(chunks)))
 
-                output = self.model.generate(**safe_inputs, max_new_tokens=256)
-                text = self.processor.decode(output, skip_special_tokens=True, audio_chunk_index=audio_chunk_index, language="nl")[0]
+        text = self.model.transcribe(
+            audio=inputs,
+            language=[CONFIG['language']] * len(chunks)
+        )
+        process_time = (time.perf_counter() - timer)
+        seconds = int(process_time % 60)
+        print(f"[QWN] <  Transcription completed in {int(process_time/60)}:{'0' if seconds < 10 else ''}{seconds} minutes")
 
-                process_time = (time.perf_counter() - timer)
-                print(f"< Transcription completed in {int(process_time/60)}:{int(process_time % 60)} minutes")
+        transcript = ' '.join([t.text.strip() for t in text])
 
-                transcripts[row['name']] = text
-                times[row['name']] = round(process_time/60, 3)
-
-        return transcripts, times
+        return transcript, process_time
 
 
-class WhisperAsrCpu(BaseModel):
+class WhisperAsrFast(BaseModel):
     def __init__(self):
         super().__init__(
-            name="Whisper-large-v3-cpu-optimized",
+            name="Whisper-large-v3-fast",
             model=WhisperModel(
                 'large-v3',
-                device=device,
-                compute_type="int8",
-                cpu_threads=4
+                device=CONFIG['device'],
+                # compute_type=CONFIG['dtype']
+                compute_type='int8'
             )
         )
+        print('[WPF] v  Model initialized and loaded in succesfully')
 
-    def transcribe(self, dataset: 'DS') -> (dict[str: str], dict[str: float]):
-        transcripts = {}
-        times = {}
+    def transcribe(self, data_file: dict) -> (str, float):
 
-        for row in dataset.data:
-            timer = time.perf_counter()
-
-            print(f"> Attempting to transcribe {row['name']} from dataset {dataset.name}")
-            text, info = self.model.transcribe(row['audio']['array'])
-            process_time = (time.perf_counter() - timer)
-            print(f"< Transcription completed in {int(process_time/60)}:{int(process_time % 60)} minutes")
-            
-            text = " ".join([segment.text.strip() for segment in text])
-
-            transcripts[row['name']] = text
-            times[row['name']] = round(process_time/60, 3)
-
-        return transcripts, times
-
-
-# --------------- Whisper-large-v3 ----------------
-class WhisperAsr(BaseModel):
-    def __init__(self):
-
-        super().__init__(
-            name="Whisper-large-v3",
-            model=AutoModelForSpeechSeq2Seq.from_pretrained(
-                "openai/whisper-large-v3",
-                dtype=torch_type,
-                low_cpu_mem_usage=True
-            ),
-            processor=AutoProcessor.from_pretrained("openai/whisper-large-v3")
+        timer = time.perf_counter()
+        print(f"[WPF] >  Attempting to transcribe {data_file['name']} from dataset {data_file['category']}")
+        text, info = self.model.transcribe(
+            data_file['audio'],
+            beam_size=CONFIG['beam_size']
         )
+        process_time = (time.perf_counter() - timer)
+        seconds = int(process_time % 60)
+        print(f"[WPF] <  Transcription completed in {int(process_time/60)}:{'0' if seconds < 10 else ''}{seconds} minutes")
 
-        self.pipe = pipeline(
-            "automatic-speech-recognition",
-            model=self.model,
-            tokenizer=self.processor.tokenizer,
-            feature_extractor=self.processor.feature_extractor,
-            chunk_length_s=30,
-            batch_size=batch_size,
-            torch_dtype=torch_type,
-            device=device,
-        )
+        transcript = " ".join([segment.text.strip() for segment in text])
 
-        # print(self.pipe.model.generation_config)
-
-    def transcribe(self, dataset) -> (dict[str: str], dict[str: float]):
-
-        transcripts = {}
-        times = {}
-
-        for row in dataset.data:
-            timer = time.perf_counter()
-            print(f"> Attempting to transcribe {row['name']} from dataset {dataset.name}")
-            text = self.pipe(row['audio']['array'],
-                             return_timestamps=True,
-                             generate_kwargs={"language": "nl",
-                                              "task": "transcribe"})['text']
-            process_time = (time.perf_counter() - timer)
-            print(f"< Transcription completed in {int(process_time/60)}:{int(process_time % 60)} minutes")
-
-            transcripts[row['name']] = text
-            times[row['name']] = round(process_time/60, 3)
-
-        return transcripts, times
-
-
-# --------------- NVIDIA Parakeet TDT 0.6B v3 ----------------
-class ParakeetAsr(BaseModel):
-    def __init__(self):
-        super().__init__(
-            name="NVIDIA Parakeet TDT 0.6B v3",
-            model=ASRModel.from_pretrained(model_name="nvidia/parakeet-tdt-0.6b-v3")
-        )
-        self.model.to(dtype=torch_type)
-        self.model.change_attention_model(self_attention_model="rel_pos_local_attn", att_context_size=[256, 256])
-
-    def transcribe(self, dataset: 'DS') -> (dict[str: str], dict[str: float]):
-
-        transcripts = {}
-        times = {}
-
-        for row in dataset.data:
-            timer = time.perf_counter()
-            audio = row['audio']['array']
-
-            print(f"> Attempting to transcribe {row['name']} from dataset {dataset.name}")
-            text = self.model.transcribe(audio)
-
-            process_time = (time.perf_counter() - timer)
-            print(f"< Transcription completed in {int(process_time/60)}:{int(process_time % 60)} minutes")
-
-            transcripts[row['name']] = text[0].text
-            times[row['name']] = round(process_time/60, 3)
-
-        return transcripts, times
-
-
-# --------------- NVIDIA Canary 1B v2 ----------------
-class CanaryAsr(BaseModel):
-    def __init__(self):
-        super().__init__(
-            name="NVIDIA Canary 1B v2",
-            model=ASRModel.from_pretrained(model_name="nvidia/canary-1b-v2")
-        )
-        self.model.to(dtype=torch_type)
-
-    def transcribe(self, dataset: 'DS'):
-
-        transcripts = {}
-        times = {}
-
-        for row in dataset.data:
-            timer = time.perf_counter()
-            print(f"> Attempting to transcribe {row['name']} from dataset {dataset.name}")
-            audio = row['audio']['array']
-            text = self.model.transcribe(audio, source_lang='nl', target_lang='nl')
-
-            process_time = (time.perf_counter() - timer)
-            print(f"< Transcription completed in {int(process_time/60)}:{int(process_time % 60)} minutes")
-
-            transcripts[row['name']] = text[0].text
-            times[row['name']] = round(process_time/60, 3)
-
-        return transcripts, times
-
-# ----------------------- mms-1b-all -----------------------
-# class MmsAllAsr(AsrModel):
-#     def __init__(self):
-#         super().__init__(
-#             name="mms-1b-all",
-#             model=
-#         )
-
-
-for mod in [WhisperAsrCpu]:
-    m = mod()
-    print(m.name)
-    output = m.transcribe(DS("Test"))
-    print(output)
+        return transcript, process_time
---
+++ asr_models_cohere.py
@@ -0,0 +1,90 @@
+import os
+from base_model import BaseModel, CONFIG
+from transformers import AutoProcessor, CohereAsrForConditionalGeneration, VibeVoiceAsrForConditionalGeneration
+import time
+from pathlib import Path
+import librosa
+
+
+# --------------- Cohere Labs Transcribe ----------------
+class CohereAsr(BaseModel):
+    def __init__(self):
+        super().__init__(
+            name="Cohere Labs Transcribe",
+            model=CohereAsrForConditionalGeneration.from_pretrained(
+                "CohereLabs/cohere-transcribe-03-2026",
+                device_map=CONFIG['device'],
+                dtype=CONFIG['dtype']
+            ),
+            processor=AutoProcessor.from_pretrained("CohereLabs/cohere-transcribe-03-2026")
+        )
+        print('[CHR] v  Model initialized and loaded in succesfully')
+
+    def transcribe(self, data_file) -> (str, float):
+
+        timer = time.perf_counter()
+
+        print(f"[CHR] >  Attempting to transcribe {data_file['name']} from dataset {data_file['category']}")
+        inputs = self.processor(data_file['audio'], sampling_rate=CONFIG['sample_rate'], return_tensors='pt', language='nl')
+        inputs = inputs.to(self.model.device, dtype=self.model.dtype)
+        audio_chunk_index = inputs.get("audio_chunk_index")
+        output = self.model.generate(**inputs, max_new_tokens=CONFIG['max_new_tokens'])
+        text = self.processor.decode(output, skip_special_tokens=True, audio_chunk_index=audio_chunk_index, language="nl")[0]
+
+        process_time = (time.perf_counter() - timer)
+        seconds = int(process_time % 60)
+        print(f"[CHR] <  Transcription completed in {int(process_time/60)}:{'0' if seconds < 10 else ''}{seconds} minutes")
+
+        transcript = text
+
+        return transcript, process_time
+
+
+class VibeVoiceAsr(BaseModel):
+    def __init__(self):
+        super().__init__(
+            name="Microsoft VibeVoice",
+            model=VibeVoiceAsrForConditionalGeneration.from_pretrained(
+                "microsoft/VibeVoice-ASR-HF",
+                device_map="auto",
+                dtype=CONFIG['dtype']
+            ),
+            processor=AutoProcessor.from_pretrained("microsoft/VibeVoice-ASR-HF")
+        )
+
+    def transcribe(self, data_file: dict) -> (str, float):
+
+        timer = time.perf_counter()
+
+        print(f"[VBV] >  Attempting to transcribe {data_file['name']} from dataset {data_file['category']}")
+
+        inputs = self.processor.apply_transcription_request(audio=data_file['audio'])
+        inputs = inputs.to(self.model.device, self.model.dtype)
+
+        output_ids = self.model.generate(**inputs)
+        generated_ids = output_ids[:, inputs["input_ids"].shape[1]:]
+
+        transcript = self.processor.decode(generated_ids, return_format="transcription_only")[0]
+
+        process_time = (time.perf_counter() - timer)
+        seconds = int(process_time % 60)
+        print(f"[VBV] <  Transcription completed in {int(process_time/60)}:{'0' if seconds < 10 else ''}{seconds} minutes")
+
+        return transcript, process_time
+
+
+for mod in [VibeVoiceAsr]:
+    m = mod()
+    print(m.name)
+    p = Path("dataset/Test/audio")
+
+    for file in os.listdir(p):
+        audio, _ = librosa.load(p / file, sr=16000, mono=True)
+        b = {
+            'name': file,
+            'category': 'Test',
+            'audio': audio
+        }
+
+        t = m.run(b)
+        print(t)
